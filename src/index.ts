91bcc36a1bbaba5c2078e75afff0d7d6b6e410cc
sameer dhulap <28100769+sameerdhulap@users.noreply.github.com>
2024-11-14 15:21:51 +0530

    plugin update passing event attribute to batch

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,4 @@
-import { ConfigPlugin, createRunOncePlugin } from "@expo/config-plugins";
-
 import WoosmapExpoPluginGeofencingBatchModule from "./WoosmapExpoPluginGeofencingBatchModule";
-import { withAndroidSdk } from "./configWoosmapGeofencingAndroid";
-import { withIOSSdk } from "./configWoosmapGeofencingIOS";
-import { ConfigProps } from "./types";
-
-const withWoosmapGeofencingPlugin: ConfigPlugin<ConfigProps> = (
-  config,
-  _props,
-) => {
-  const props = _props || { apiKey: "" };
-
-  config = withAndroidSdk(config, props);
-  config = withIOSSdk(config, props);
-
-  return config;
-};
-const pkg = require("../package.json");
-export default createRunOncePlugin(
-  withWoosmapGeofencingPlugin,
-  pkg.name,
-  pkg.version,
-);
 
 // Reexport the native module. On web, it will be resolved to WoosmapExpoPluginGeofencingBatchModule.web.ts
 // and on native platforms to WoosmapExpoPluginGeofencingBatchModule.ts
