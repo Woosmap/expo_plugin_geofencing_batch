231d209584a418c55f9a1466c81bd95215f71fd9
sameer dhulap <28100769+sameerdhulap@users.noreply.github.com>
2024-11-14 18:09:32 +0530

    Update

--- src/nativeSetup.ts
+++ src/nativeSetup.ts
@@ -1,6 +1,5 @@
 import { ConfigPlugin, createRunOncePlugin } from "@expo/config-plugins";
 
-//import WoosmapExpoPluginGeofencingBatchModule from "./WoosmapExpoPluginGeofencingBatchModule";
 import { withAndroidSdk } from "./configWoosmapGeofencingAndroid";
 import { withIOSSdk } from "./configWoosmapGeofencingIOS";
 import { ConfigProps } from "./types";
@@ -22,10 +21,3 @@ export default createRunOncePlugin(
   pkg.name,
   pkg.version,
 );
-
-// Reexport the native module. On web, it will be resolved to WoosmapExpoPluginGeofencingBatchModule.web.ts
-// and on native platforms to WoosmapExpoPluginGeofencingBatchModule.ts
-
-// export function getApiKey(): string {
-//   return WoosmapExpoPluginGeofencingBatchModule.getApiKey();
-// }
