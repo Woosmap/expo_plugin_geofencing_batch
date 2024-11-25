0b622e020852e0283781f037d263c5b5d90f7331
Amit Kale <akale@webgeoservices.com>
2024-11-25 15:13:42 +0530

    removed test methods

--- android/src/main/java/expo/modules/geofencingbatchplugin/WoosmapExpoLifecycleListener.kt
+++ android/src/main/java/expo/modules/geofencingbatchplugin/WoosmapExpoLifecycleListener.kt
@@ -2,66 +2,14 @@ package expo.modules.geofencingbatchplugin
 
 import android.app.Application
 import android.content.Context
-import android.content.Intent
 import android.content.IntentFilter
 import android.os.Build
-import com.batch.android.Batch
-import com.batch.android.BatchActivityLifecycleHelper
-import com.webgeoservices.woosmapgeofencing.PositionsManager
-import com.webgeoservices.woosmapgeofencing.WoosmapSettings
-import com.webgeoservices.woosmapgeofencingcore.database.WoosmapDb
 import expo.modules.core.interfaces.ApplicationLifecycleListener
-import org.json.JSONObject
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 
 class WoosmapExpoLifecycleListener : ApplicationLifecycleListener {
-    private var positionsManager: PositionsManager? = null
-    private var woosmapDb: WoosmapDb? = null
-    private val executorService: ExecutorService = Executors.newSingleThreadExecutor()
-
     override fun onCreate(application: Application) {
         super.onCreate(application)
-        WoosmapSettings.privateKeyWoosmapAPI = ""
-        WoosmapSettings.searchAPIEnable = true
-
-        executorService.execute {
-            woosmapDb = WoosmapDb.getInstance(application.applicationContext)
-            val dbInstance = woosmapDb
-            positionsManager = dbInstance?.let {
-                PositionsManager(application.applicationContext,
-                    it
-                )
-            }
-            positionsManager?.searchAPI(19.211771554276368, 72.8646941303407, -1)
-
-            startBatch(application)
-            registerBroadcastReceiver(application)
-
-            try {
-                Thread.sleep(2000)
-                val intent = getIntent()
-                application.sendBroadcast(intent)
-            } catch (ignored: Exception) {
-            }
-        }
-    }
-
-    private fun getIntent(): Intent {
-        val data = JSONObject().apply {
-            put("identifier", "dhruv_apt")
-            put("eventname", "entered")
-            put("longitude", 72.8646941303407)
-            put("latitude", 19.211771554276368)
-            put("date", System.currentTimeMillis())
-            put("didenter", true)
-            put("radius", 100.0)
-            put("frompositiondetection", true)
-            put("spenttime", 23)
-        }
-        return Intent("com.woosmap.action.GEOFENCE_TRIGGERED").apply {
-            putExtra("regionLog", data.toString())
-        }
+        registerBroadcastReceiver(application)
     }
 
     private fun registerBroadcastReceiver(application: Application) {
@@ -70,10 +18,5 @@ class WoosmapExpoLifecycleListener : ApplicationLifecycleListener {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             application.registerReceiver(receiver, filter, Context.RECEIVER_EXPORTED)
         }
-    }
-
-    private fun startBatch(application: Application) {
-        Batch.start("")
-        application.registerActivityLifecycleCallbacks(BatchActivityLifecycleHelper())
     }
 }
