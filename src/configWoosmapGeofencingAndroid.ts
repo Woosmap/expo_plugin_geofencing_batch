00ae6ce96b45a0eda563d8df53531739853aa6b2
sameer dhulap <28100769+sameerdhulap@users.noreply.github.com>
2025-06-10 17:05:21 +0530

    #13 remove location permission

--- src/configWoosmapGeofencingAndroid.ts
+++ src/configWoosmapGeofencingAndroid.ts
@@ -5,7 +5,6 @@ import {
 } from "@expo/config-plugins";
 
 import { ConfigProps } from "./types";
-import { addMetaDataItemToMainApplication } from "@expo/config-plugins/build/android/Manifest";
 
 const withSDKAndroidManifest: ConfigPlugin<ConfigProps> = (config, props) => {
   config = withAndroidManifest(config, (config) => {
@@ -13,41 +12,28 @@ const withSDKAndroidManifest: ConfigPlugin<ConfigProps> = (config, props) => {
       config.modResults,
     );
 
-    const {
+    var {
       useAndroidBackgroundLocation,
       useAndroidBluetooth,
     } = props;
-
+   useAndroidBackgroundLocation =  useAndroidBackgroundLocation == undefined? true: useAndroidBackgroundLocation; //Default true
+   useAndroidBluetooth =  useAndroidBluetooth == undefined? true: useAndroidBluetooth; //Default true
+    
     // Add location and BLE permissions to the <manifest> tag
     const permissions = [
       "android.permission.ACCESS_COARSE_LOCATION",
       "android.permission.ACCESS_FINE_LOCATION",
-      "android.permission.ACCESS_BACKGROUND_LOCATION",
-      "android.permission.BLUETOOTH",
-      "android.permission.BLUETOOTH_ADMIN",
-      "android.permission.BLUETOOTH_SCAN",
     ];
-    if(useAndroidBackgroundLocation == null){
-      if(useAndroidBackgroundLocation == true){
-        permissions.push("android.permission.ACCESS_BACKGROUND_LOCATION")
-      }
-    } else{
-        permissions.push("android.permission.ACCESS_BACKGROUND_LOCATION")
+   
+    if(useAndroidBackgroundLocation == true){
+      permissions.push("android.permission.ACCESS_BACKGROUND_LOCATION")
     }
     
-
-    if(useAndroidBluetooth == null){
-      if(useAndroidBluetooth == true){
-        permissions.push("android.permission.BLUETOOTH")
-        permissions.push("android.permission.BLUETOOTH_ADMIN")
-        permissions.push("android.permission.BLUETOOTH_SCAN")
-      }
-    } else{
-        permissions.push("android.permission.BLUETOOTH")
-        permissions.push("android.permission.BLUETOOTH_ADMIN")
-        permissions.push("android.permission.BLUETOOTH_SCAN")
+    if(useAndroidBluetooth == true){
+      permissions.push("android.permission.BLUETOOTH")
+      permissions.push("android.permission.BLUETOOTH_ADMIN")
+      permissions.push("android.permission.BLUETOOTH_SCAN")
     }
-
     
     // Ensure each permission is added only once
     permissions.forEach((permission) => {
@@ -71,8 +57,5 @@ const withSDKAndroidManifest: ConfigPlugin<ConfigProps> = (config, props) => {
 
 export const withAndroidSdk: ConfigPlugin<ConfigProps> = (config, props) => {
   config = withSDKAndroidManifest(config, props);
-  //   config = withSDKEntitlements(config, props);
-  //   config = withSDKXcodeProject(config, props);
-  //   config = withSDKDangerousMod(config, props);
   return config;
 };
